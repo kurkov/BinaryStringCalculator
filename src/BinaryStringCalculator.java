489877fb139e231530de99ee0331b3beb468a464
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-06-06 00:30:10 +0300

    made tests and fixed algorithm implementation

--- src/BinaryStringCalculator.java
+++ src/BinaryStringCalculator.java
@@ -1,18 +1,32 @@
 public class BinaryStringCalculator {
+
   public static String add(String s1, String s2) {
+
     StringBuilder result = new StringBuilder();
+    String num1, num2;
+
+    int sum, cursorS1, cursorS2;
+    int maxCharCount;
     int memory = 0;
-    int countChars = 0;
-    if (s1.length() >= s2.length()){
-      countChars = s1.length();
-    } else {
-      countChars = s2.length();
+
+    cursorS1 = s1.length() - 1;
+    cursorS2 = s2.length() - 1;
+
+    String s1Check = checkIfStringIsValid(s1, cursorS1);
+    String s2Check = checkIfStringIsValid(s2, cursorS2);
+    if (s1Check.equals("-1") || s2Check.equals("-1")) {
+      return "Error! Invalid binary string";
     }
-    int num1, num2, sum;
-    for (int i = countChars; i > 0; i--) {
-      num1 = Integer.parseInt(String.valueOf(s1.charAt(i)));
-      num2 = Integer.parseInt(String.valueOf(s2.charAt(i)));
-      sum = num1 + num2 + memory;
+
+    maxCharCount = getMaxCharCount(cursorS1, cursorS2);
+
+    for (int i = maxCharCount; i >= 0; i--) {
+
+      num1 = getStringChar(s1, cursorS1);
+      num2 = getStringChar(s2, cursorS2);
+
+      sum = Integer.parseInt(num1) + Integer.parseInt(num2) + memory;
+
       if (sum == 0) {
         result.append("0");
         memory = 0;
@@ -20,13 +34,57 @@ public class BinaryStringCalculator {
         result.append("1");
         memory = 0;
       } else if (sum == 2) {
-        result.append("1");
-        memory = 0;
-      } else {
+        result.append("0");
+        memory = 1;
+      } else if (sum == 3) {
         result.append("1");
         memory = 1;
+      } else {
+        result = new StringBuilder("Error! Enter valid binary string");
+        break;
+      }
+
+      cursorS1--;
+      cursorS2--;
+
+      if ((i == 0) && (memory == 1)) {
+        result.append("1");
       }
     }
-    return result.toString();
+    return result.reverse().toString();
+  }
+
+  private static String getStringChar(String s1, int cursorS1) {
+    String num1;
+    if (cursorS1 >= 0) {
+      num1 = String.valueOf(s1.charAt(cursorS1));
+    } else {
+      num1 = "0";
+    }
+    return num1;
+  }
+
+  private static int getMaxCharCount(int cursorS1, int cursorS2) {
+    int maxCharCount;
+    if (cursorS1 >= cursorS2) {
+      maxCharCount = cursorS1;
+    } else {
+      maxCharCount = cursorS2;
+    }
+    return maxCharCount;
+  }
+
+  private static String checkIfStringIsValid(String string, int cursor) {
+
+    String number, result = "";
+
+    for (int i = 0; i < cursor; i++) {
+      number = String.valueOf(string.charAt(cursor));
+      if (!number.equals("0") && !number.equals("1")) {
+        result = "-1";
+        break;
+      }
+    }
+    return result;
   }
 }
--- src/TestString.java
+++ src/TestString.java
@@ -9,9 +9,38 @@ public class TestString {
   // "100"
   
   @Test
-  public void testString(){
+  public void test1_EqualLengthStrings(){
     String expectedResult = "100";
     String actualResult = BinaryStringCalculator.add("01", "11");
+    assertEquals(expectedResult, actualResult);
+  }
+
+  //  "111"
+  //   "01"
+  // "1000"
+
+  @Test
+  public void test2_DifferentLengthStrings(){
+    String expectedResult = "1000";
+    String actualResult = BinaryStringCalculator.add("111", "01");
+    assertEquals(expectedResult, actualResult);
+  }
+
+  //   "01"
+  // "1011"
+  // "1100"
+
+  @Test
+  public void test3_DifferentLengthStrings(){
+    String expectedResult = "1100";
+    String actualResult = BinaryStringCalculator.add("01", "1011");
+    assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  public void test4_InvalidBinaryString(){
+    String expectedResult = "Error! Invalid binary string";
+    String actualResult = BinaryStringCalculator.add("02", "1011");
     assertEquals(expectedResult, actualResult);
   }
 }
